73649a85d205641336a4364ac95d56439f6d6d60
DanyaLitvinov <vfvdaniel07@gmail.com>
2023-10-03 19:01:19 +0300

    123123

--- src/js/fetchphoto.js
+++ src/js/fetchphoto.js
@@ -5,6 +5,8 @@ export default async function fetchImages(name, page, per_page) {
     const response = await axios.get(
       `https://pixabay.com/api/?key=31350060-6fd355912bbf3f893372e3348&q=${name}&page=${page}&per_page=${per_page}&image_type=photo&orientation=horizontal&safesearch=true`
     );
+
+    
     return response;
   } catch (error) {
     console.error(error);
